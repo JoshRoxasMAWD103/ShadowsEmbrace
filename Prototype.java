957e6bef837d103a166a88486b932be654644fa9
JoshRoxas <aaronjoshuaroxas678@gmail.com>
2020-12-13 01:56:41 +0800

    Update Prototype.java

--- Prototype.java
+++ Prototype.java
@@ -1,188 +1,335 @@
 import java.util.Scanner;
 import java.util.Random;
-public class ShadowsEmbrace 
+public class ShadowsEmbrace
 {
-	
 	//SYSTEM VARIABLES
 	Random r = new Random();
 	Scanner s = new Scanner(System.in);
+	Scanner enterScanner = new Scanner(System.in);
 	
 	//PLAYER VARIABLES
-	int playerHP = 100;
-	int maxPlayerHP = r.nextInt(300);
-	String playerName;
-	int playerDamage = r.nextInt(25);
-	int maxPlayerDamage;
-	int choice;
-	int role;
-	String playerWeapon;
+	String playerName, choice, role, playerWeapon, playerClass, playerSkills;
+	int maxPlayerHP, maxPlayerMana, maxPlayerArmor, 
+		hpAdd, manaAdd, armorAdd, 
+		playerHP, playerMana, playerArmor;
+	int playerDamage;
+	int currentPlayerDamage;
 	
 	//OTHER GAME VARIABLES
 	int monsterHP;
-	String monster[] = {"Skeleton", "Ghoul", "Wraith", "Zombie","Vampire",};
-        String monsterRandomizer = monster[r.nextInt(monster.length)];
-	int monsterCounter = 3;
+	int monsterDamage;
+	String monster[] = {"Skeleton", "Ghoul", "Wraith", "Zombie", "Vampire",};
+        String monsterRandomizer;
+	int monsterCounter = 5;
 	int abbadonDamage = 40;
 	int abbadonHP = 100;
+	int monstercounter = 4; //5th one will be a boss
 	
-	int asmodeusHP = 150;
-	
+	int asmodeusHP;
+	int asmodeusDamage;
 	
 	//ITEMS
-	
 	int healLeft = 3;
 	int revive; // Used to finish the game
 	int key;
 		
 	public static void main(String[] args) 
 	{
-
 		ShadowsEmbrace game;
-		game = new ShadowsEmbrace();	
+		game = new ShadowsEmbrace();
 		
-		game.playerSetUp(); 	
+		game.playerSetUp();
 		game.startUp();
+		game.firstMonsterWave();
 	}
 	
-	public void playerSetUp(){
-		
-	        System.out.println("\t------------------------------------------------"); 
-	        System.out.println("\n\tWelcome to Shadow's Embrace.");
-	        System.out.print("\tYour adventure begins here. Please enter a name: ");
+	public void playerSetUp()
+	{
+	    System.out.println("\nWelcome to Shadow's Embrace.");
+	    System.out.print("Your adventure begins here. Please enter a name: ");
 		playerName = s.nextLine();
-		System.out.println("\t------------------------------------------------"); 
+		System.out.println("------------------------------------------------"); 
 		
-		System.out.print("\n\tHello " + playerName + ", let's pick a role!");
+		System.out.print("Hello " + playerName + ", let's pick a role!");
 		System.out.println("\n\t1. Warrior 	");
 		System.out.println("\t2. Mage");
 		System.out.println("\t3. Archer");
 		System.out.print("Select: ");
-		
-		role = s.nextInt();
-		
-		System.out.println("\t------------------------------------------------"); 
+		role = s.nextLine();
+		System.out.println("\n------------------------------------------------"); 
 		
 		//ROLES OR CLASSES TO SELECT
-		
-		if (role == 1)
+		if (role.equals("1"))
 		{
-			maxPlayerHP = 300;
-			maxPlayerDamage = 40;
+			//WARRIOR CHOICE
+			playerHP = 10;
+			maxPlayerHP = 220;
+			hpAdd = 15;
 			
-			System.out.println("Class: Warrior.");
+			playerMana = 0;
+			maxPlayerMana = 80;
+			manaAdd = 5;
+			
+			playerArmor = 5;
+			maxPlayerArmor = 145;
+			armorAdd = 10;
+			
+			playerClass = "Warrior";
 			playerWeapon = "Long Sword";
-			playerHP = playerHP + r.nextInt(maxPlayerHP);
-			playerDamage = playerDamage + r.nextInt(maxPlayerDamage);
-			
-			
-			System.out.println("Player Weapon: " + playerWeapon);
-			System.out.println("Player HP: " + playerHP);
-			System.out.println("Possible Damage: " + playerDamage);
-			System.out.println("Health Potions Left: " + healLeft);
-			
-		
+			playerSkills = "\n\t[1]Punch \n\t[2]Blade Surge \n\t[3]Hail of Blades";
 		}
 		
-		else if (role == 2)
+		else if (role.equals("2"))
 		{
-			maxPlayerHP = r.nextInt(140);
-			maxPlayerDamage = r.nextInt(100);
+			//MAGE CHOICE
+			playerHP = 10;
+			maxPlayerHP = 150;
+			hpAdd = 10;
 			
-			System.out.println("Class: Mage.");
+			playerMana = 20;
+			maxPlayerMana = 220;
+			manaAdd = 15;
+			
+			playerArmor = 5;
+			maxPlayerArmor = 75;
+			armorAdd = 5;
+			
+			playerClass = "Mage";
 			playerWeapon = "Staff";
-			playerDamage = playerDamage + maxPlayerDamage;
-			playerHP = playerHP + maxPlayerHP;
-			System.out.println("Player Weapon: " + playerWeapon);
-			System.out.println("Player HP: " + playerHP);
-			System.out.println("Possible Damage: " + playerDamage);
-			System.out.println("Health Potions Left: " + healLeft);
-			
-		
+			playerSkills = "\n\t[]Fireball \n\t[2]Thunderbolt \n\t[3]Hinokami Kagura";
 		}
 		
-		else if (role == 3)
+		else if (role.equals("3"))
 		{
-			maxPlayerHP = r.nextInt(200);
-			maxPlayerDamage = r.nextInt(75);
+			//ARCHER CHOICE
+			playerHP = 10;
+			maxPlayerHP = 150;
+			hpAdd = 10;
 			
-			System.out.println("Class: Archer.");
+			playerMana = 5;
+			maxPlayerMana = 145;
+			manaAdd = 10;
+			
+			playerArmor = 10;
+			maxPlayerArmor = 80;
+			armorAdd = 5;
+			
+			playerClass = "Archer";
 			playerWeapon = "Bow";
-			playerDamage = playerDamage + maxPlayerDamage;
-			playerHP = playerHP + maxPlayerHP;
-			System.out.println("Player Weapon: " + playerWeapon);
-			System.out.println("Player HP: " + playerHP);
-			System.out.println("Possible Damage: " + playerDamage);
-			System.out.println("Health Potions Left: " + healLeft);
-		}
-		
-		else
-		{
-			System.out.println("Invalid Command");
-		}
-	}	
-	
-	public void startUp(){
-		
-		System.out.println("\n------------------------------------------------------------------\n");
-		System.out.println(playerName +" You are a man fighting for Nessa. You must rescue her in order for you to successfully finish this mission!");
-		System.out.println("But beware of dangerous underground creatures that you may encounter!");
-		System.out.println("You need to defeat 3 boss in order for you to sucessfully retrieve her");
-		System.out.println("What do you want to do?");
-		System.out.println("");
-		System.out.println("[1]: Continue Walking");
-		System.out.println("[2]: Exit Game");
-		System.out.println("\n------------------------------------------------------------------\n");
-		System.out.print("Select: ");
-		choice = s.nextInt();
-		
-		if(choice==1)
-		{
-			firstMonsterWave();		
-		}
-
-		else if(choice==2)
-		{
-			System.out.println("You left the game.");
-		}
-		
-		else
-		{
-			System.out.println("Invalid Command");
-			startUp();
+			playerSkills = "\n\t[1]Arrow Shot \n\t[2]Sectral Arrow \n\t[3]Enchanted Arrow";
 		}
 	}
 	
+	public void startUp()
+	{
+		System.out.println("Class: " + playerClass);
+		System.out.println("Player HP: " + playerHP);
+		System.out.println("Player Weapon: " + playerWeapon);
+		System.out.println("Player Skills: " + playerSkills);
+		System.out.println("Health Potions Left: " + healLeft);
+			
+		System.out.println("\n------------------------------------------------------------------\n");
+		System.out.println(playerName + " You are a man fighting for Nessa. You must rescue her in order for you to successfully finish this mission!");
+		System.out.println("But beware of dangerous underground creatures that you may encounter!");
+		System.out.println("You need to defeat 3 boss in order for you to sucessfully retrieve her");
+		System.out.println("\tPress Enter to Continue");
+		choice = s.nextLine();
+		System.out.println("\n------------------------------------------------------------------\n");
+	}
 	
-
+	
 	public void firstMonsterWave()
 	{
 		while (monsterCounter > 0)
 		{
 			monsterCounter--;
 			System.out.println("\n------------------------------------------------------------------\n");
-			System.out.println("You encountered a " + monsterRandomizer + "!\n");
-			System.out.println("Choose:");
-			System.out.println("[1:] Fight");
-			System.out.print("Select: ");
-			choice = s.nextInt();
-			System.out.println("\n------------------------------------------------------------------\n");
+			monsterRandomizer = monster[r.nextInt(monster.length)];
+			System.out.println("You encountered a " + monsterRandomizer + "!");
 			
-			if(choice==1)
+			//GHOUL STATS
+			if (monsterRandomizer.equals("Ghoul"))
 			{
-				fight();
+				int monsterHP = r.nextInt(50);
+				if (monsterHP <= 30)
+				{
+					monsterHP = 30;
+				}
+			
+				int monsterDamage = r.nextInt(30);
+				if (monsterDamage <= 20)
+				{
+					monsterDamage = 20;
+				}
+			
+				System.out.println("\tMonster HP: " + monsterHP + "\n\tMonster Damage: " + monsterDamage);
+				monsterFight();
 			}
-			
-			else 
+			//SKELETON STATS
+			else if (monsterRandomizer.equals("Skeleton"))
 			{
-				firstMonsterWave();
+				int monsterHP = r.nextInt(55);
+				if (monsterHP <= 25)
+				{
+					monsterHP = 25;
+				}
+				
+				int monsterDamage = r.nextInt(50);
+				if (monsterDamage <= 40)
+				{
+					monsterDamage = 40;
+				}
+				
+				System.out.println("\tMonster HP: " + monsterHP + "\n\tMonster Damage: " + monsterDamage);
+				monsterFight();
+			}
+			//WRAITH STATS
+			else if (monsterRandomizer.equals("Wraith"))
+			{
+				int monsterHP = r.nextInt(50);
+				if (monsterHP <= 35)
+				{
+					monsterHP = 35;
+				}
+				
+				int monsterDamage = r.nextInt(40);
+				if (monsterDamage <= 35)
+				{
+					monsterDamage = 35;
+				}
+				
+				System.out.println("\tMonster HP: " + monsterHP + "\n\tMonster Damage: " + monsterDamage);
+				monsterFight();
+			}
+			//ZOMBIE STATS
+			else if (monsterRandomizer.equals("Zombie"))
+			{
+				int monsterHP = r.nextInt(55);
+				if (monsterHP <= 35)
+				{
+					monsterHP = 35;
+				}
+				
+				int monsterDamage = r.nextInt(35);
+				if (monsterDamage <= 25)
+				{
+					monsterDamage = 25;
+				}
+				
+				System.out.println("\tMonster HP: " + monsterHP + "\n\tMonster Damage: " + monsterDamage);
+				monsterFight();
+			}
+			//VAMPIRE STATS
+			else if (monsterRandomizer.equals("Vampire"))
+			{
+				int monsterHP = r.nextInt(40);
+				if (monsterHP <= 10)
+				{
+					monsterHP = 10;
+				}
+				
+				int monsterDamage = r.nextInt(60);
+				if (monsterDamage <= 50)
+				{
+					monsterDamage = 50;
+				}
+				
+				System.out.println("\tMonster HP: " + monsterHP + "\n\tMonster Damage: " + monsterDamage);
+				monsterFight();
 			}
 		}
 	}
 	
 	
-	public void fight()
+	public void monsterFight()
 	{
-		monsterRandomizer = monster[r.nextInt(monster.length)];
+		System.out.println(">Your HP: " + playerHP);
+		
+		if (monsterHP > 0)
+		{
+			System.out.println(">" + monsterRandomizer + "'s HP: " + monsterHP);
+		}
+		else if (monsterHP <= 0)
+		{
+			System.out.println(">" + monsterRandomizer + "'s HP: " + 0);
+		}
+		
+		System.out.println("Choose:");
+		System.out.println("\n\t[1]: Attack");
+		System.out.println("\t[2]: Use an item.");
+		System.out.println("\n------------------------------------------------------------------\n");
+		System.out.print("Select: ");
+		choice = s.nextLine();
+		
+		if(choice.equals("1"))
+		{
+			monsterBattleMode();
+		}
+		
+		else if(choice.equals("2"))
+		{
+			if (healLeft == 0)
+			{
+				System.out.println("You have used all of your heals!");
+				monsterFight();
+			}
+			heal();
+		}
+		
+		else
+		{
+			System.out.println("Invalid Command!");
+		}
+	}
+	
+	
+	//OLD ATTACK PUBLIC VOID
+	public void monsterBattleMode()
+	{
+		currentPlayerDamage = r.nextInt(playerDamage);
+		if (currentPlayerDamage <= 25)
+		{
+			currentPlayerDamage = 25;
+		}
+		playerHP = playerHP - monsterDamage;
+		monsterHP = monsterHP - currentPlayerDamage;
+		System.out.println("You attacked the monster and dealt " + currentPlayerDamage + " damage!");
+		System.out.println("The monster attacked you for " + monsterDamage);
+		
+		/*System.out.println("\nMonster HP: " + monsterHP + );
+		System.out.println("Your HP: " + playerHP);
+		System.out.println("Please enter (1) to continue");
+		choice = s.nextInt();
+		
+		if(monsterCounter<=0 && monsterHP <= 0)
+		{
+			win(); 
+		} 
+		
+		else if (monsterHP <= 0)
+		{
+			System.out.println("You defeated the " + monsterRandomizer + "!");
+			fight();
+		}
+		else if(monsterHP > 0)
+		{
+			attack();
+			
+			if(playerHP <= 0)
+			{ 
+				dead(); 
+			} 
+			
+			else if(playerHP > 0)
+			{
+				attack();
+			}
+		}*/
+		
+	}
+	
+	/*public void monsterstats()
+	{
 		//GHOUL STATS
 		if (monsterRandomizer.equals("Ghoul"))
 		{
@@ -199,6 +346,7 @@ public class ShadowsEmbrace
 			}
 			
 			System.out.println("\tMonster HP: " + monsterHP + "\n\tMonster Damage: " + monsterDamage);
+			monsterFight();
 		}
 		//SKELETON STATS
 		else if (monsterRandomizer.equals("Skeleton"))
@@ -216,6 +364,7 @@ public class ShadowsEmbrace
 			}
 			
 			System.out.println("\tMonster HP: " + monsterHP + "\n\tMonster Damage: " + monsterDamage);
+			monsterFight();
 		}
 		//WRAITH STATS
 		else if (monsterRandomizer.equals("Wraith"))
@@ -233,6 +382,7 @@ public class ShadowsEmbrace
 			}
 			
 			System.out.println("\tMonster HP: " + monsterHP + "\n\tMonster Damage: " + monsterDamage);
+			monsterFight();
 		}
 		//ZOMBIE STATS
 		else if (monsterRandomizer.equals("Zombie"))
@@ -250,6 +400,7 @@ public class ShadowsEmbrace
 			}
 			
 			System.out.println("\tMonster HP: " + monsterHP + "\n\tMonster Damage: " + monsterDamage);
+			monsterFight();
 		}
 		//VAMPIRE STATS
 		else if (monsterRandomizer.equals("Vampire"))
@@ -267,94 +418,16 @@ public class ShadowsEmbrace
 			}
 			
 			System.out.println("\tMonster HP: " + monsterHP + "\n\tMonster Damage: " + monsterDamage);
+			monsterFight();
 		}
+	}*/
 		
-		System.out.println("\n------------------------------------------------------------------\n");
-		System.out.println("\t>Your HP: "+ playerHP);
-
-		
-		System.out.println("Choose:");
-		System.out.println("\n[1:] Attack");
-		System.out.println("[2:] Use an item.");
-		System.out.println("\n------------------------------------------------------------------\n");
-		System.out.print("Select: ");
-		choice = s.nextInt();
-		
-		if(choice==1)
-		{
-			attack();
-		}
-		
-		else if(choice==2)
-		{
-			if (healLeft == 0)
-			{
-				System.out.println("You have used all of your heals!");
-				fight();
-			}
-			heal();
-		}
-		
-		else
-		{
-			fight();
-		}
-		
-		//USED SO THAT THE MONSTER HP WON'T BE NEGATIVE
-		if (monsterHP <= 0) 
-		{
-			monsterHP = r.nextInt(60);
-		}
-	}
-	
-	public void attack()
-	{
-		int monsterDamage =  r.nextInt(25);
-		playerHP = playerHP - monsterDamage;
-		
-		
-		System.out.println("");
-		System.out.println("There are " + monsterCounter + " enemies left!");
-		System.out.println("You attacked the monster and dealt " + playerDamage + " damage!");
-		System.out.println("The monster attacked you for " + monsterDamage);
-		monsterHP = monsterHP - playerDamage;
-		
-		System.out.println("\nMonster HP: " + monsterHP);
-		System.out.println("Your HP: " + playerHP);
-		System.out.println("Please enter (1) to continue");
-		
-		if(monsterCounter<=0 && monsterHP <= 0)
-		{
-			win(); 
-		} 
-		
-		else if(monsterHP > 0)
-		{
-			fight();
-			
-			if(playerHP <= 0)
-			{ 
-				dead(); 
-			} 
-			
-			else if(playerHP>0)
-			{
-				fight();
-			}
-		}
-		else if(playerHP<=0)
-			{ 
-				dead(); 
-			} 
-	}
-	
 	public void dead()
 	{
 		System.out.println("\n------------------------------------------------------------------\n");
 		System.out.println("You are dead!!!");
 		System.out.println("\n\nGAME OVER");
 		System.out.println("\n------------------------------------------------------------------\n");
-		
 	}
 	
 	public void win()
@@ -362,8 +435,8 @@ public class ShadowsEmbrace
 		if(monsterCounter == 0)
 		{
 		System.out.println("\n------------------------------------------------------------------\n");
-		System.out.println("You killed all the monster!");
-		System.out.println("The monster dropped a key to the dungeon!");
+		System.out.println("You killed the wave of monster!");
+		System.out.println("The " + monsterRandomizer + " dropped a key to the dungeon!");
 		System.out.println("You obtained a key!\n\n");
 		System.out.println("1: Continue to the dungeon");
 		System.out.println("\n------------------------------------------------------------------\n");
@@ -373,8 +446,8 @@ public class ShadowsEmbrace
 		
 		System.out.print("Select: ");
 		
-		choice = s.nextInt();
-		if(choice==1)
+		choice = s.nextLine();
+		if(choice.equals("1"))
 		{
 			firstBossFight();
 		}
@@ -389,16 +462,31 @@ public class ShadowsEmbrace
 	{
 		if (healLeft > 0)
 		{
-		healLeft--;
-		playerHP = playerHP + 50;
-		System.out.println("You used heal. You have " + healLeft + " left");
-		System.out.println("Current HP is " + playerHP);
-		fight();
+			if (playerHP == playerHP)
+			{
+				System.out.println("You are still at full health!");
+				monsterFight();
+			}
+			else if (playerHP >= (playerHP - 50))
+			{
+				healLeft--;
+				playerHP += 50;
+				System.out.println("You used heal. You have " + healLeft + " left");
+				System.out.println("Current HP is maxed out to " + playerHP);
+				monsterFight();
+			}
+			else if (playerHP < (playerHP - 50))
+			{
+				healLeft--;
+				playerHP += 50;
+				System.out.println("You used heal. You have " + healLeft + " left");
+				System.out.println("Current HP is" + playerHP);
+				monsterFight();
+			}
 		}
 		else 
 		{
 			System.out.println("You have used all your heal!");
-			fight();
 			
 		}
 		
@@ -406,73 +494,23 @@ public class ShadowsEmbrace
 	
 	public void firstBossFight()
 	{
-			
-		if(abbadonHP > 0)
-		{
 			System.out.println("\nYou encountered the first boss Abbadon");
 			System.out.println("\n------------------------------------------------------------------\n");
 			System.out.println("\t >BOSS FIGHT!");
 			System.out.println("\t>Your HP: "+ playerHP);
 			System.out.println("\t>Abbadon's HP: " + abbadonHP);
 			System.out.println("Choose:");
-			System.out.println("[1.] Fight");
+			System.out.println("[1.] Continue");
 			System.out.println("[2.] Heal");
 			System.out.println("\n------------------------------------------------------------------\n");
 			System.out.print("Select: ");
-			choice = s.nextInt();	
-			
-			
-			if (choice == 1)
-			{
-				bossAttack();
-			}
-			
-			else if (choice == 2)
-			{
-				healLeft--;
-				playerHP = playerHP + 50;
-				System.out.println("You have used your heal potion! Your HP is " + playerHP);
-				System.out.println("Heals Left: " + healLeft);
-				firstBossFight();
-			}
-			
-			else 
-			{
-				System.out.println("Invalid Command!");
-				firstBossFight();
-			}
-		}
-		
-		
-		else if (abbadonHP <= 0)
-		{
-			healLeft = healLeft +3;
-			System.out.println("\nYou killed Abbadon! You recieved additional 3 heal potions!");
-			ending();
-		}
-	
+			choice = s.nextLine();	
 	}
 	
 	
 	
 		
-		public void bossAttack()
-		{
-			if (abbadonHP > 0)
-			{
-			abbadonHP = abbadonHP - playerDamage;
-			playerHP = playerHP - abbadonDamage;
-			
-			
-			System.out.println("\nAbbadon attacked you for " + abbadonDamage + " damage!");
-			System.out.println("You attacked Abbadon for " + playerDamage + " damage!");
-			
-			
-			firstBossFight();
-			}
-			
-			
-		}
+		
 	
 	
 	
